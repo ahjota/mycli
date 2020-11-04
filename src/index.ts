880e332a181ef891305024368c99a6b88b460ee9
AJ Alon <aj.alon@datarobot.com>
2020-11-03 16:10:36 -0800

    open DR app, prompt for API token

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Command, flags } from '@oclif/command'
 import * as inquirer from 'inquirer'
+import open from 'open'
 
 // https://api-docs.datarobot.com/docs/guide-to-different-datarobot-endpoints
 class Ajcli extends Command {
@@ -28,12 +29,13 @@ class Ajcli extends Command {
     let deployment = flags.deployment
     let endpoint = flags.endpoint
     let deploymentRoot = "https://app2.datarobot.com"
+    let drDeveloperToolsUrl = deploymentRoot
 
     if (!endpoint) {
       if (!deployment) {
         let responses: any = await inquirer.prompt([{
           name: "deployment",
-          message: "Select your DataRobot deployment",
+          message: "What is your DataRobot deployment?",
           type: "list",
           choices: [
             { name: 'Managed AI Cloud', value: 'cloud' },
@@ -72,11 +74,53 @@ class Ajcli extends Command {
       }
 
       endpoint = deploymentRoot.concat("/api/v2")
+      drDeveloperToolsUrl = deploymentRoot.concat("/account/developer-tools")
     }
 
-    this.log(`You are authenticating to the DataRobot API at ${endpoint}`)
+    // TODO Bypass if token already exists in configuration
+    let apiAccessHowTo: string = "https://api-docs.datarobot.com/docs/api-access-guide"
+    let loginInstructions: string =
+      `Hello! You are authenticating to the DataRobot API at ${endpoint}.
+You'll need an API token, so let's get you one!
 
+In a moment, I'm going to take you to the DataRobot app at ${deploymentRoot}.
+You'll need to sign up or login, and then you'll want to go to Developer Tools
+and create a new API key. It's pretty easy, but the how-to can be found at
+${apiAccessHowTo}.
+
+Copy that API key, then come back here and paste it in at the next prompt.`
+
+    this.log(loginInstructions)
+    let confirmLogonResponse: any = await inquirer.prompt([{
+      name: "loginconfirm",
+      message: "Ready to login to DataRobot?",
+      type: "confirm",
+    }])
+    if (confirmLogonResponse.loginconfirm) {
+      // TODO Sadly DR doesn't have the ability to sticky a particular page
+      // TODO node's `open` package requires you to close the whole browser
+      await open(drDeveloperToolsUrl)
+      await sleep(2000)
+    }
+
+    let saveTokenResponse: any = await inquirer.prompt([{
+      name: "apiToken",
+      message: "What is your API token?",
+      type: "password",
+    }])
+
+    let apiToken: string = saveTokenResponse.apiToken
+    this.log("Let's make sure that token works...")
+
+    // GET https://app.datarobot.com/api/v2/projects/ HTTP/1.1
+    this.log("... and you're in. 🔥")
   }
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 export = Ajcli
